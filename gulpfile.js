50e59c330ff8b0bde186f97ad698225f7964ed8e
Sebastian Sandqvist <seb@sparque.me>
2015-06-21 14:48:18 -0700

    update dev dependencies and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
 var lint = require('gulp-jshint');
 
 // ----- lint
 // ---------------------------------------
-gulp.task('lint', function() {
-    return gulp.src('index.js')
-        .pipe(lint('etc/.jshintrc'))
-        .pipe(lint.reporter('jshint-stylish'));
-});
-
-// ----- unit test after linting
-// ---------------------------------------
-gulp.task('default', ['lint'], function () {
-    return gulp.src('test/*.js', { read: false})
-        .pipe(mocha({ reporter: 'spec' }));
+gulp.task('default', function() {
+	return gulp.src('index.js')
+		.pipe(lint('etc/.jshintrc'))
+		.pipe(lint.reporter('jshint-stylish'));
 });

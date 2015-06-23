ee22880e8cae4a28bf66ffaf7f709cc9be7389be
Sebastian Sandqvist <seb@sparque.me>
2015-06-22 20:45:05 -0700

    s-query rename, full rewrite

--- index.js
+++ index.js
@@ -1,243 +1,110 @@
 'use strict';
 
-
 // ----- main constructor
-//		--	el {HTMLElement} optional DOM element (or HTMLCollection)
+//		--	@param el {Array} HTMLElement array
 // ---------------------------------------
-var Dom = function(el) {
+var Query = function(el) {
 	this.el = el;
 };
 
 
+Query.prototype = {
 
-// --------------------------------- all methods ---------------------------------
-
-
-// ----- return DOM element
-// ---------------------------------------
-Dom.prototype.dom = function() {
-	return this.el;
-};
+	constructor: Query,
 
 
-// ----- create DOM element
-// ---------------------------------------
-Dom.prototype.create = function(str) {
-
-	this.el = document.createElement(str);
-	document.body.appendChild(this.el);
-
-	return this;
-
-};
-
-
-// ----- remove DOM element(s)
-// ---------------------------------------
-Dom.prototype.remove = function() {
-
-	if (!this.el) {
-		return this;
-	}
-
-	var removed;
-
-	// HTMLCollection/NodeList
-	if (this.el.length) {
-
-		removed = [];
-
-		for (var i=0, el, removedEl; i<this.el.length; i++) {
-			el = this.el[i];
-			removedEl = el.parentNode.removeChild(el);
-			removed.push(removedEl);
-		}
-
-	}
-	// HTMLElement/Node
-	else {
-		removed = this.el.parentNode.removeChild(this.el);
-	}
-
-	return removed;
-
-};
-
-
-// ----- append
-// ---------------------------------------
-Dom.prototype.append = function(str) {
-
-	var newElement = document.createElement(str);
-
-	this.el.appendChild(newElement);
-	this.el = newElement;
-
-	return this;
-
-};
-
-
-// ----- select, get/set id
-// ---------------------------------------
-Dom.prototype.id = function(value) {
-
-	// as a getter
-	if (this.el && !value) {
-		return this.el.id;
-	}
-
-	// as a selector
-	if (!this.el && value) {
-		this.el = document.getElementById(value);
-	}
-
-	// as a setter
-	if (this.el && value) {
-		this.el.id = value;
-	}
-
-	return this;
-
-};
-
-
-// ----- get/set attributes
-//		--	@param attribute {String} attribute to get/set
-//		--	@param value {String} optional value to set
-// ---------------------------------------
-Dom.prototype.attr = function(attribute, value) {
+	// --------------------------------- core ---------------------------------
 	
-	if (arguments.length === 1) {
-		return this.el.getAttribute(attribute);
-	}
+	each: function(cb) {
 
-	// HTMLCollection/NodeList
-	if (this.el.length) {
-		for (var i=0, len=this.el.length; i<len; i++) {
-			this.el[i].setAttribute(attribute, value);
+		for (var i = 0, len = this.el.length; i < len; i++) {
+			cb(this.el[i], i);
 		}
+
+		return this;
+
+	},
+
+
+	// --------------------------------- manipulation ---------------------------------
+	
+	append: function(str) {
+
+		this.each(function(el) {
+			el.appendChild(document.createElement(str).cloneNode(true)); // append copy
+		});
+
+		return this;
+
 	}
-	// HTMLElement/Node
-	else {
-		this.el.setAttribute(attribute, value);
+
+	// --------------------------------- traversal ---------------------------------
+	
+
+
+}; // end Query.prototype
+
+
+
+
+
+
+// --------------------------------- helpers ---------------------------------
+
+// ----- convert HTMLCollection to Array
+//		--	@param nodeList {NodeList|HTMLCollection}
+//		--	@return {Array}
+// based on http://stackoverflow.com/q/3199588/4459340
+// ---------------------------------------
+function toArray(nodeList) {
+
+	var i = nodeList.length;
+	var arr = [];
+	
+	while(i--) {
+		arr.unshift(nodeList[i]);
 	}
 
+	return arr;
 
-	return this;
-
-};
-
-
-// ----- get/set properties
-//		--	@param property {String}
-//		--	@param value {Boolean} optional
-// ---------------------------------------
-
-
-
-// ----- traversal: parent
-// ---------------------------------------
-Dom.prototype.parent = function() {
-
-	this.el = this.el.parentNode;
-	return this;
-
-};
-
-
-// ----- manipulate classes
-// ---------------------------------------
-/*Dom.prototype.addClass = function(className) {
-	this.el.classList.add(className);
-	return this;
-};
-
-Dom.prototype.removeClass = function(className) {
-	this.el.classList.remove(className);
-	return this;
-};
-
-Dom.prototype.toggleClass = function(className) {
-	this.el.classList.toggle(className);
-	return this;
 }
-
-Dom.prototype.hasClass = function(className) {
-	return this.el.classList.contains(className);
-};*/
-
-
-// --------------------------------------------------
-// TODO: add .each to core and use it for all methods
-//				instead of manually checking length of el
-// --------------------------------------------------
-
 
 
 // --------------------------------- exports ---------------------------------
 
-
-// ----------------------------------------------------------------------
-// ----- core: exported object and selector engine
-//		1. instantiate a new Dom([el]) object to get all methods
-//		2. selector engine sets `el` to a HTMLElement or HTMLCollection
-//		2.0 convert NodeLists with a single item in them into a single Node
-//		2.1 tagName, returns live HTMLCollection
-//		2.2 complex query, returns non-live NodeList
-//		2.3 id, returns a non-live HTMLElement
-//		2.4 className, returns a live HTMLCollection (NodeList in jsDom)
-//		2.5 other queries (:checked)
-// ----------------------------------------------------------------------
+// ----- export factory & selector engine
+//		--	@param? selector {String} css-like selector
+//		--	@return {Query} new s-query object
+// ---------------------------------------
 module.exports = function(selector) {
-
-	var el;
-
-	// [2]
-	if (selector)	{
-
-		// [2.0]
-		var listToEl = function (nodeList) {
-
-			if (nodeList.length === 1) {
-				return nodeList[0];
-			}
-
-			return nodeList;
-
-		};
-
-		var valid = /^[-\w]+$/; // matches hyphens, underscores, alphaNumeric strings
-		var query = selector.slice(1);
-
-		// [2.1] element(s)
-		if (valid.test(selector)) {
-			el = listToEl(document.getElementsByTagName(selector));
-		}
-		
-		// [2.2] query
-		else if (!valid.test(query)) {
-			el = listToEl(document.querySelectorAll(selector));
-		}
-
-		// [2.3] id
-		else if (selector[0] === '#') {
-			el = document.getElementById(query);
-		}
-
-		// [2.4] class
-		else if (selector[0] === '.') {
-			el = listToEl(document.getElementsByClassName(query));
-		}
-
-		// [2.5] all the rest...
-		else {
-			el = listToEl(document.querySelectorAll(selector));
-		}
-
-		
-	} // end if (selector)
-
-	return new Dom(el); // [1];
-
+	var arr = toArray(document.querySelectorAll(selector));
+	return new Query(arr);
 };
+
+
+// ----- selector engine
+//		--	@param id|className|tagName|name {String}
+//		--	@return {Query}
+// ---------------------------------------
+module.exports.byId = function(id) {
+	return new Query([document.getElementById(id)]);
+};
+
+module.exports.byClass = function(className) {
+	var elements = toArray(document.getElementsByClassName(className));
+	return new Query(elements);
+};
+
+module.exports.byTag = function(tag) {
+	var elements = toArray(document.getElementsByTagName(tag));
+	return new Query(elements);
+};
+
+module.exports.byName = function(name) {
+	var elements = toArray(document.getElementsByName(name));
+	return new Query(elements);
+};
+
+
+// attach helper functions
+module.exports.toArray = toArray;
--- test/spec.js
+++ test/spec.js
@@ -15,206 +15,195 @@ require('blanket')({
 });
 
 var expect = require('chai').expect;
-var dom = require('../index.js');
+var query = require('../index.js');
 
 
-// ----- tests
+// --------------------------------- tests ---------------------------------
+
+// ----- default selector engine
 // ---------------------------------------
-describe('dom', function() {
+describe('default selector engine', function() {
 
-	it('should return a DOM element', function() {
-		var domDiv = dom().create('div').dom();
-		expect(domDiv).to.be.an.instanceof(window.HTMLElement);
-		expect(domDiv).to.be.an.instanceof(window.Node);
-		dom().create('div');
-		var domDivs = dom('div').dom();
-		expect(domDivs).to.be.an.instanceof(window.NodeList);
-		dom('div').remove();
-	});
+	// select by id and class is sufficient to show
+	// that querySelectorAll is correctly in place
+	it('should select by ID', function() {
 
-});
+		// setup
+		var el = document.createElement('div');
+		el.id = 'selectById';
+		document.body.appendChild(el);
 
+		// test
+		expect(query('#selectById').el[0]).to.be.an.instanceof(window.HTMLElement);
 
-describe('create', function() {
+		// teardown
+		el.parentNode.removeChild(el);
 
-	it('should create an element and append it to body', function() {
-		var div = dom().create('div');
-		expect(div.el).to.be.an.instanceof(window.HTMLElement)
-		expect(div.el).to.be.an.instanceof(window.Node)
-		expect(div.el._localName).to.equal('div');
-		div.remove();
-	});
-
-	it('should be possible to have two distinct elements', function() {
-		var span1 = dom().create('span').id('span1');
-		var span2 = dom().create('span').id('span2');
-		expect(span1.el).to.not.deep.equal(span2.el); // note: DOM deep equal takes a long time
-		span1.remove();
-		span2.remove();
-	});
-
-});
-
-
-describe('remove', function() {
-
-	it('should return the removed element and remove that element', function() {
-		var div2 = dom().create('div').id('removedDiv');
-		expect(document.documentElement.innerHTML).to.include('removedDiv');
-		var oldDiv = div2.remove();
-		expect(oldDiv).to.be.an.instanceof(window.HTMLElement);
-		expect(oldDiv).to.be.an.instanceof(window.Node);
-		expect(document.documentElement.innerHTML).to.not.include('removedDiv');
-	});
-
-	it('should also work with HTMLCollections', function() {
-		var a1 = dom().create('a').id('a1');
-		var a2 = dom().create('a').id('a2');
-		var a3 = dom().create('a').id('a3');
-		expect(document.documentElement.innerHTML).to.include('a1');
-		expect(document.documentElement.innerHTML).to.include('a2');
-		expect(document.documentElement.innerHTML).to.include('a3');
-		var removed = dom('a').remove();
-		expect(document.documentElement.innerHTML).to.not.include('a1');
-		expect(document.documentElement.innerHTML).to.not.include('a2');
-		expect(document.documentElement.innerHTML).to.not.include('a3');
-	});
-
-	it('should do nothing if there is nothing to remove', function() {
-		var nothing = dom().remove();
-		expect(nothing.el).to.be.undefined;
-		nothing.create('span').id('fromNothing');
-		expect(nothing.el).to.not.be.undefined;
-		expect(nothing.el.id).to.equal('fromNothing');
-		nothing.remove();
-	});
-
-});
-
-
-describe('append', function() {
-
-	it('should append an element to an element', function() {
-		var appended = dom().create('div').id('parent').append('span').id('child');
-		expect(appended.el.id).to.equal('child');
-		expect(appended.parent().el.id).to.equal('parent');
-	});
-	
-	it('should also work with DOM elements')
-	it('should also work with DOM collections/nodeLists')
-	it('should set current element to appended element')
-
-});
-
-
-describe('parent', function() {
-
-	it('should traverse to the parent')
-
-});
-
-
-describe('selector', function() {
-
-	it('should select by id', function() {
-		var span3 = dom().create('span').id('span3');
-		expect(dom('#span3').el._localName).to.equal('span');
-		expect(dom('#span3').el.id).to.equal('span3');
-		span3.remove();
-	});
-
-	it('should select by a complex query (attribute selector)', function() {
-		var b = dom().create('b').attr('data-test', 'testing');
-		var query = dom('[data-test]');
-		var query2 = dom('[data-test=testing]');
-		var query3 = dom('[data-test^=test]');
-		expect(query).to.deep.equal(b);
-		expect(query2).to.deep.equal(b);
-		expect(query3).to.deep.equal(b);
-		b.remove();
-	});
-
-	it('should select by a complex query (child selector)', function() {
-		var form = dom().create('form').id('form1');
-		var input = dom().create('input').id('input1');
-		form.el.appendChild(input.el);
-		var query = dom('form input');
-		expect(query).to.deep.equal(input);
-		expect(query.el).to.be.an.instanceof(window.HTMLElement)
-		expect(query.el).to.be.an.instanceof(window.Node)
-		input.remove();
-		form.remove();
 	});
 
 	it('should select by className', function() {
-		var a1 = dom().create('a').attr('class', 'foo').id('a1');
-		var a2 = dom().create('a').attr('class', 'foo').id('a2');
-		var a3 = dom().create('a').attr('class', 'foo').id('a3');
-		var a4 = dom().create('a').attr('class', 'Foo').id('a4');
-		var foos = dom('.foo');
-		expect(foos.el.length).to.equal(3);
-		expect(foos.el).to.be.an.instanceof(window.NodeList)
-		expect(foos.el[0].className).to.equal('foo');
-		expect(foos.el[1].className).to.equal('foo');
-		expect(foos.el[2].className).to.equal('foo');
-		foos.remove();
-		a4.remove();
+
+		// setup
+		var el = document.createElement('div');
+		el.className = 'selectByClassName';
+		document.body.appendChild(el);
+
+		// test
+		expect(query('.selectByClassName').el[0]).to.be.an.instanceof(window.HTMLElement);
+
+		// teardown
+		el.parentNode.removeChild(el);
+
+	});
+
+});
+
+
+describe('manual selector engine', function() {
+
+	it('should select by id', function() {
+
+		// setup
+		var el = document.createElement('div');
+		el.id = 'selectById';
+		document.body.appendChild(el);
+
+		// test
+		expect(query.byId('selectById').el[0]).to.be.an.instanceof(window.HTMLElement);
+
+		// teardown
+		el.parentNode.removeChild(el);
+
+	});
+
+	it('should select by className', function() {
+
+		// setup
+		var el = document.createElement('div');
+		el.className = 'selectByClassName';
+		document.body.appendChild(el);
+
+		// test
+		expect(query.byClass('selectByClassName').el[0]).to.be.an.instanceof(window.HTMLElement);
+
+		// teardown
+		el.parentNode.removeChild(el);
+
 	});
 
 	it('should select by tagName', function() {
-		var em = dom().create('em').id('em1');
-		expect(dom('em').el._localName).to.equal('em');
-		expect(dom('em').el.id).to.equal('em1');
-		em.remove();
+
+		// setup
+		var el = document.createElement('ul');
+		document.body.appendChild(el);
+
+		// test
+		expect(query.byTag('ul').el[0]).to.be.an.instanceof(window.HTMLElement);
+
+		// teardown
+		el.parentNode.removeChild(el);
+
 	});
 
-	it('should select by state selector', function() {
-		var checkbox = dom().create('input').attr('type', 'checkbox')
-		checkbox.dom().checked = true;
-		var query = dom(':checked');
-		expect(query.el).to.be.an.instanceof(window.HTMLElement);
-		checkbox.remove();
-	});
+	it('should select by name', function() {
 
-});
+		// setup
+		var el = document.createElement('textarea');
+		el.setAttribute('name', 'selectByName');
+		document.body.appendChild(el);
 
+		// test
+		expect(query.byName('selectByName').el[0]).to.be.an.instanceof(window.HTMLElement);
 
+		// teardown
+		el.parentNode.removeChild(el);
 
-describe('id', function() {
-
-	it('should work as a getter & setter', function() {
-		var span4 = dom().create('span').id('span4');
-		expect(span4.id()).to.equal('span4');
-		expect(span4.el.id).to.equal('span4');
-		span4.remove();
-	});
-
-	it('should work as a selector', function() {
-		dom().create('span').id('span5');
-		var span5 = dom().id('span5');
-		expect(span5.el.id).to.equal('span5');
-		expect(span5.el instanceof window.HTMLElement).to.be.true;
-		expect(span5.el instanceof window.Node).to.be.true;
-		span5.remove();
 	});
 
 });
 
 
-describe('attr', function() {
+// ----- append
+// ---------------------------------------
+describe('append', function() {
 
-	it('should work as a getter and setter', function() {
-		var span = dom().create('span').id('span6').attr('data-test', 'testing');
-		expect(dom('#span6').attr('data-test')).to.equal('testing');
-		span.remove();
+	it('should append an element to an element by tagName', function() {
+
+		// setup
+		var appended = document.createElement('div');
+		appended.id = 'parent';
+		document.body.appendChild(appended);
+		query('#parent').append('span');
+		var child = query('span').el[0];
+		var html = document.documentElement.innerHTML;
+
+		// test
+		expect(child.parentNode.id).to.equal('parent');
+		expect(html).to.include('<div id="parent"><span></span></div>');
+
+		// teardown
+		appended.parentNode.removeChild(appended);
+
 	});
 
-	it('should set multiple elements\' attributes', function() {
-		var span7 = dom().create('span').id('span7');
-		var span8 = dom().create('span').id('span8');
-		dom('span').attr('data-test2', 'testing2');
-		expect(span7.attr('data-test2')).to.equal('testing2');
-		expect(span8.attr('data-test2')).to.equal('testing2');
+	it('should append an element to multiple elements by tagName', function() {
+
+		// setup
+		var appended1 = document.createElement('div');
+		var appended2 = document.createElement('h1');
+		appended1.className = 'parent';
+		appended2.className = 'parent';
+		document.body.appendChild(appended1);
+		document.body.appendChild(appended2);
+		query('.parent').append('span');
+		var html = document.documentElement.innerHTML;
+
+		// test
+		var expected = '<div class="parent"><span></span></div>' + 
+									 '<h1 class="parent"><span></span></h1>';
+		expect(html).to.include(expected);
+
+		// teardown
+		appended1.parentNode.removeChild(appended1);
+		appended2.parentNode.removeChild(appended2);
+
+	});
+
+});
+
+
+// ----- each
+// ---------------------------------------
+describe('each', function() {
+	it('0 matches: should do nothing');
+	it('1 match: should call function for matched item');
+	it('2 matches: should call function for matched items');
+});
+
+
+// ----- toArray
+// ---------------------------------------
+describe('toArray', function() {
+
+	it('should nondestructively convert a NodeList to an Array', function() {
+
+		// setup
+		var div1 = document.createElement('div');
+		var div2 = document.createElement('div');
+		var body = document.body;
+		body.appendChild(div1);
+		body.appendChild(div2);
+
+		// test
+		expect(query.toArray(body.children)).to.not.be.an.instanceof(window.NodeList);
+		expect(query.toArray(body.children)).to.be.an.instanceof(Array);
+		expect(body.children).to.be.an.instanceof(window.NodeList);
+		expect(body.children.length).to.equal(2); // proof it's nondestructive
+
+		// teardown
+		div1.parentNode.removeChild(div1);
+		div2.parentNode.removeChild(div2);
+
 	});
 
 });
